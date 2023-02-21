314ca46cbc70637dc8411e694d6e97a343143779
masha8709 <masha.8709@mail.ru>
2023-02-21 12:31:25 +0300

    header для mobile и загрузка фото для ретины

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+let navigation = document.querySelector('.navigation');
+let navToggle = document.querySelector('.nav__button-toggle');
+
+navigation.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navigation.classList.contains('nav--closed')) {
+    navigation.classList.remove('nav--closed');
+    navigation.classList.add('nav--opened');
+  } else {
+    navigation.classList.add('nav--closed');
+    navigation.classList.remove('nav--opened');
+  }
+});
