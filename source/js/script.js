769cc35df31aaf7490c904bb5ca5422bc14aedc9
masha8709 <masha.8709@mail.ru>
2023-02-22 11:58:27 +0300

    Карта leaflet

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,6 @@
 /* в этот файл добавляет скрипты*/
+'use strict'
+
 let navigation = document.querySelector('.navigation');
 let navToggle = document.querySelector('.nav__button-toggle');
 
@@ -13,3 +15,27 @@ navToggle.addEventListener('click', function () {
     navigation.classList.remove('nav--opened');
   }
 });
+
+
+
+////////КАРТА
+
+const map = L.map('map')
+  .setView([59.968338, 30.317366], 17);
+
+L.tileLayer(
+  'https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '(c) OSM contributors',
+  },
+).addTo(map);
+
+const pinIcon = L.icon({
+  iconUrl: './img/map/map-pin.svg',
+  iconSize: [38, 50],
+  iconAnchor: [0, 50],
+});
+
+const pinMarker = L.marker([59.968338, 30.317366], {
+    icon: pinIcon
+  })
+  .addTo(map);
