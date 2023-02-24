17b31f330ecda45e023b26a0821ea491fc170740
masha8709 <masha.8709@mail.ru>
2023-02-24 11:25:11 +0300

    Слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -63,3 +63,48 @@ sortList.addEventListener('click', (evt) => {
   sortSelectButton.classList.remove('sort__list--open');
   sortSelectButton.textContent = sortSelectType.textContent;
 });
+
+
+/////Слайдер
+
+const slider = document.querySelector('.slider__list');
+const sliderItems = Array.from(slider.children);
+const btnNext = document.querySelector('.slider__button-next');
+const btnPrev = document.querySelector('.slider__button-prev');
+
+sliderItems.forEach(function (slide, index) {
+	if (index !== 0) slide.classList.add('hidden');
+	slide.dataset.index = index;
+
+	sliderItems[0].setAttribute('data-active', '');
+
+	slide.addEventListener('click', function () {
+		showNextSlide('next');
+	});
+});
+
+btnNext.onclick = function () {
+	showNextSlide('next');
+};
+
+btnPrev.onclick = function () {
+	showNextSlide('prev');
+};
+
+function showNextSlide(direction) {
+	const currentSlide = slider.querySelector('[data-active]');
+	const currentSlideIndex = +currentSlide.dataset.index;
+	currentSlide.classList.add('hidden');
+	currentSlide.removeAttribute('data-active');
+
+	let nextSlideIndex;
+	if (direction === 'next') {
+		nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
+	} else if (direction === 'prev') {
+		nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+	}
+
+	const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+	nextSlide.classList.remove('hidden');
+	nextSlide.setAttribute('data-active', '');
+}
